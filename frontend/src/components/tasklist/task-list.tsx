9223bc58c2d9e0e2a941cdc055181752206ddcf6
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-10-03 22:57:09 +0200

    (frontend) refactor move Task and TaskList to components/taskList
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

---
+++ frontend/src/components/tasklist/task-list.tsx
@@ -0,0 +1,106 @@
+"use client"
+
+import { StrictMode, useEffect, useMemo, useRef } from "react";
+import { AnimatePresence } from "motion/react";
+import { useMounted } from "@/hooks/use-mounted";
+
+import { taskListConfig } from "@/config/taskList";
+
+import { useAppDispatch, useAppSelector, useAppStore } from "@/hooks/redux";
+import { addTask, getNextTaskKey, load, saveTaskListState, taskListSlice } from "@/lib/features/taskList/slice";
+import { selectLastActionType } from "@/lib/store";
+
+import { Task } from "./task";
+import { Collapse } from "../collapse";
+import { Button } from "../ui/button";
+import { Plus } from "lucide-react";
+
+export function TaskList() {
+  const store = useAppStore();
+  const initialized = useRef(false);
+  if (!initialized.current) {
+    initialized.current = true;
+    store.dispatch(load());
+    store.subscribe(() => {
+      saveTaskListState(store.getState().taskList);
+    });
+  }
+
+  const dispatch = useAppDispatch();
+
+  const tasks = useAppSelector(state => state.taskList.tasks);
+  const lastActionType = useAppSelector(selectLastActionType);
+
+  const searchFilter = useAppSelector(state => state.taskListFilter.search);
+  const completionFilter = useAppSelector(state => state.taskListFilter.completion);
+  const priorityFilter = useAppSelector(state => state.taskListFilter.priority);
+
+  const lastTaskRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    /* Focus the text area of the last task once a new one has been added. */
+    if (lastActionType !== taskListSlice.actions.addTask.type) return;
+    interface Focusable extends HTMLElement { focus: () => void }
+    (lastTaskRef.current?.lastChild as Focusable).focus();
+  }, [lastActionType])
+
+  const mounted = useMounted();
+
+  const tasksComponent = useMemo(() => {
+    let sortedTasks = tasks;
+    if (priorityFilter !== "none") {
+      sortedTasks = tasks.toSorted((a, b) => {
+        if (priorityFilter === "asc") return a.priority - b.priority;
+        else return b.priority - a.priority;
+      });
+    }
+
+    return (
+      <div key={0} className="flex flex-col w-full">
+        {sortedTasks.map((task, index) => {
+          if (!task.name.includes(searchFilter)) return;
+          else if (completionFilter === "done" && !task.completed) return;
+          else if (completionFilter === "undone" && task.completed) return;
+          return (
+            <Task
+              key={task.key}
+              index={index}
+              data={task}
+              inputRef={index === tasks.length - 1 ? lastTaskRef : null}
+            />
+          );
+        })}
+      </div>
+    );
+  }, [searchFilter, completionFilter, priorityFilter, tasks]);
+
+  return mounted ? (
+    <StrictMode>
+      <AnimatePresence>
+        <div className="flex flex-col w-full">
+          {tasksComponent}
+          <Collapse
+            /* Avoid adding multiple sentinel tasks elements. */
+            show={!Boolean(tasks.length) || Boolean(tasks[tasks.length - 1].name)}
+            key={1}
+            layout
+          >
+            <Button
+              variant="outline"
+              className="opacity-50 shadow-none border-none bg-transparent dark:bg-transparent h-7 w-full text-muted-foreground hover:opacity-100 text-sm w-full"
+              onClick={_ => {
+                dispatch(addTask({
+                  ...taskListConfig.defaultTask,
+                  /* Guarantee a unique key for each new task element. */
+                  key: getNextTaskKey(tasks),
+                }));
+              }}
+            >
+              <Plus /> Add
+            </Button>
+          </Collapse>
+        </div>
+      </AnimatePresence>
+    </StrictMode >
+  ) : null;
+}
---
+++ frontend/src/components/tasklist/task.tsx
@@ -0,0 +1,63 @@
+import { RefObject, useRef, useState } from "react";
+
+import { Collapse } from "../collapse";
+import { Checkbox } from "../ui/checkbox";
+import { TextInput } from "../text-input";
+import { TaskToolbar } from "./task-toolbar";
+
+import { cn } from "@/lib/utils";
+import { useAppDispatch } from "@/hooks/redux";
+import { changeTaskCompleted, changeTaskName } from "@/lib/features/taskList/slice";
+import { type Task } from "@/lib/features/taskList/schema";
+
+interface TaskProps {
+  index: number,
+  data: Task,
+  inputRef?: RefObject<HTMLDivElement | null> | null,
+};
+
+export function Task({ index, data, inputRef }: TaskProps) {
+  const dispatch = useAppDispatch();
+
+  const ref = useRef<HTMLTextAreaElement>(null);
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <Collapse key={data.key} layout>
+        <div ref={inputRef} className={cn(
+          "flex has-[[aria-checked=true]]:line-through has-[[aria-checked=true]]:text-muted-foreground transition-all",
+          data.indentLevel == 0 ? "pl-0" : "pl-6",
+        )}>
+          <div className="flex py-2">
+            <Checkbox
+              defaultChecked={data.completed}
+              className={data.indentLevel == 0 ? "" : "rounded-lg"}
+              onCheckedChange={state => dispatch(
+                changeTaskCompleted({ index: index, completed: state === true ? true : false })
+              )}
+            />
+          </div>
+          <TextInput
+            inputRef={ref}
+            onFocus={_ => setShow(true)}
+            onBlur={e => {
+              /* Skip updating state in store on no changes to the task name. */
+              if (data.name !== e.target.value || !e.target.value) {
+                dispatch(changeTaskName({ index: index, name: e.target.value }));
+              }
+              setShow(false);
+            }}
+            defaultValue={data.name}
+          />
+        </div>
+      </Collapse>
+      <Collapse show={show && Boolean(data.name)}>
+        <div className="absolute flex left-0 flex-col items-center w-full">
+          <TaskToolbar taskIndex={index} inputRef={ref} />
+        </div>
+      </Collapse>
+    </>
+  );
+
+}
