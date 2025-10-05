f84cbdf68d473813a8d061f3c2c1bdeaffcff6ad
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-10-05 23:09:29 +0200

    (frontend) fix register page auth flow
    * Register, then signin immediately after.
    
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -6,22 +6,35 @@ import { useRouter } from "next/navigation";
 
 import AuthForm, { AuthFormSchema } from "@/components/auth-form";
 import { toast } from "sonner";
+import api from "@/lib/api/axios";
 
 export default function Page() {
   const router = useRouter();
 
   async function onSubmit(formData: z.infer<typeof AuthFormSchema>) {
-    const res = await signIn("credentials", {
-      redirect: false,
-      email: formData.email,
-      password: formData.password,
-    });
+    try {
+      await api({
+        method: "post",
+        url: "auth/register",
+        data: {
+          email: formData.email,
+          password: formData.password,
+        },
+      });
 
-    if (res?.error) {
+      const res = await signIn("credentials", {
+        redirect: false,
+        email: formData.email,
+        password: formData.password,
+      });
+
+      if (res?.error) throw new Error(res.error);
+    } catch (error) {
       toast.error("Email already registered");
-    } else {
-      router.push("/");
+      return;
     }
+
+    router.push("/");
   }
 
   return (
