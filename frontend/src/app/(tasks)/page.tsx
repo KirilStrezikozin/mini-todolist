1f4dd05b3c25d63316573cca7724eaaa45c9e661
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-10-05 13:21:26 +0200

    (frontend) update move LoginTip on tasks page to the bottom
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- frontend/src/app/(tasks)/page.tsx
+++ frontend/src/app/(tasks)/page.tsx
@@ -59,9 +59,9 @@ export default function Page() {
         <StoreProvider>
           <div className="font-sans grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
             <main className="flex flex-col gap-[32px] items-start">
-              <LoginTip />
               <TaskListHeader />
               <TaskList />
+              <LoginTip />
             </main>
           </div >
         </StoreProvider>
